5cd8df4f773dfdf05eafaa425c43dd3abe33e7e6
Ajr Wum <ajr.wum@gmail.com>
2022-01-05 17:16:38 +0100

    finish iteration 2

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = 'mongodb://127.0.0.1:27017/recipe-app';
 
 // Connection to the database "recipe-app"
 mongoose
@@ -17,6 +17,29 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+
+    // === iteration 2
+    // - creating my recipe
+    const myRecipe = {
+      title: "Aperitive Biscuit",
+      level: "Easy Peasy",
+      ingredients: ["1 puffpastry dough", "Emmental cheese", "Cumin", "Salt", "1 egg yolk"],
+      cuisine: "French cuisine",
+      dishType: "other",
+      image: "https://www.cestmeilleurfaitmaison.fr/wp-content/uploads/2014/07/DSC_0111.jpg",
+      duration: 30,
+      creator: "AJ",
+    };
+    // - inserting this recipe into the db
+    Recipe.create(myRecipe)
+    .then((recipe) => {
+      console.log(recipe.title);
+    })
+    .catch((err) => {
+      console.error(err);
+    })
+    
+    
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
