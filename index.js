297a35c9555aeaf742302f80871308962ced2f50
Ajr Wum <ajr.wum@gmail.com>
2022-01-05 18:03:23 +0100

    finish iteration 4 + clean code

--- index.js
+++ index.js
@@ -47,15 +47,15 @@ mongoose
       }
 
       // === iteration 4
-      // - building the parameters for the request
-      const filter = { title: "Rigatoni alla Genovese" };
-      const update = { duration: 100 };
-      const option = { new: true };
-      // - updating the recipe
       try {
+        // - building the parameters for the request
+        const filter = { title: "Rigatoni alla Genovese" };
+        const update = { duration: 100 };
+        const option = { new: true };
+        // - updating the recipe
         const updatedRecipe = await Recipe.findOneAndUpdate(filter, update, option)
-          // - once done, displayed the success message
-          console.log(`Update successful! The ${updatedRecipe.title} recipe's duration is now ${updatedRecipe.duration}.`);
+        // - once done, displayed the success message
+        console.log(`Update successful! The ${updatedRecipe.title} recipe's duration is now ${updatedRecipe.duration}.`);
       }
       catch (err) {
         console.error(err);
